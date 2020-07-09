ba26d2b9d3076c42541e47403b181a4182f2d1bd
RafaS92 <manrvaldez..92@gmail.com>
2020-07-09 16:06:17 -0500

    higherComponentsImplemented

---
+++ src/hoc/withTooltip.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function withTooltip(Component) {
+  return class WithTooltip extends React.Component {
+    state = { showTooltip: false };
+
+    onMouseOut = () => {
+      this.setState({ showTooltip: false });
+    };
+
+    onMouseOver = () => {
+      this.setState({ showTooltip: true });
+    };
+
+    render() {
+      return (
+        <div onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+          <Component {...this.props} showTooltip={this.state.showTooltip} />
+        </div>
+      );
+    }
+  };
+}
+
+export default withTooltip;
